fbc26072e3d907e702943193b0e631697c044287
sorin.sbarnea@gmail.com <sorin.sbarnea@gmail.com>
2014-07-10 10:53:41 +0100

    v5 with lots of bug fixes, and more unit tests. Code coverage increased to 71%.

--- activedirectory/activedirectory.py
+++ activedirectory/activedirectory.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from __future__ import print_function
 import ConfigParser
 import logging
 import os
 import traceback
+import types
 import unittest
 from collections import defaultdict
 from urlparse import urlparse
 import ldap3
 from ldap3.utils.conv import escape_bytes
+from ldap3.protocol.rfc4511 import SearchRequest, ValsAtLeast1, Scope, Integer0ToMax, TypesOnly, Filter, AttributeSelection, Selector, EqualityMatch
 
 
 class ActiveDirectory(object):
 
-    @staticmethod
-    def factory(section, debug=False):
-        config = ConfigParser.RawConfigParser()
-        fname = os.path.splitext(__file__)[0] + ".ini"
-        config.read(fname)
-        server = config.get(section, 'hostname')
-        username = config.get(section, 'bind_username')
-        password = config.get(section, 'bind_password')
-        try:
-            basedn = config.get(section, 'base_dn')
-        except:
-            basedn = ""
-        ret = ActiveDirectory(url=server, dn=username, secret=password, base=basedn, debug=debug)
-        return ret
-
     def reconnect(self):
         self.__init__(self.url, self.dn, self.secret, base=self.base)
 
-    def __init__(self, url, dn=None, secret=None, base="", debug=False, paged_size=1000, size_limit=0):
-        """
+    def __init__(self, url, dn=None, secret=None, base="", debug=False, paged_size=1000, size_limit=None, time_limit=None):
+        """If you do not specify credentials (dn and secret) it will try to load them from ~/.netrc file.
+
         @param server: url of LDAP Server
         @param dn: username of the service account
         @param secret: password of the servce account
@@ -41,7 +30,16 @@ class ActiveDirectory(object):
         self.filter = ''
         self.scope = ldap3.SEARCH_SCOPE_WHOLE_SUBTREE
         self.paged_size = paged_size
-        self.size_limit = size_limit
+        if not size_limit:
+            self.size_limit = Integer0ToMax(0)
+        else:
+            self.size_limit = size_limit
+
+        if not time_limit:
+            self.time_limit = Integer0ToMax(0)
+        else:
+            self.time_limit = time_limit
+
         self.attrs = '*'
 
         self.logger = logging.getLogger('ldap')
@@ -118,9 +116,11 @@ class ActiveDirectory(object):
             search_scope=scope,
             attributes=attrlist,
             paged_size=self.paged_size,
-            size_limit=self.size_limit
+            size_limit=self.size_limit,
+            time_limit=self.time_limit
         )
         if self.conn.result['description'] == 'sizeLimitExceeded' or 'controls' not in self.conn.result:
+            logging.error("sizeLimitExceeded")
             cookie = None
         else:
             cookie = self.conn.result['controls']['1.2.840.113556.1.4.319']['value']['cookie']
@@ -134,6 +134,8 @@ class ActiveDirectory(object):
                 search_scope=scope,
                 attributes=attrlist,
                 paged_size=self.paged_size,
+                size_limit=self.size_limit,
+                time_limit=self.time_limit,
                 paged_cookie=cookie
             )
             if self.conn.result['description'] == 'sizeLimitExceeded':
@@ -142,6 +144,8 @@ class ActiveDirectory(object):
                 cookie = self.conn.result['controls']['1.2.840.113556.1.4.319']['value']['cookie']
             total_entries += len(self.conn.response)
             ret.extend(self.conn.response)
+            if len(ret) >= self.size_limit:
+                break
 
         # FIX for Microsoft bug: ldap.UNAVAILABLE_CRITICAL_EXTENSION: {'info': '00002040: SvcErr: DSID-031401E7, problem 5010 (UNAVAIL_EXTENSION), data 0\n', 'desc': 'Critical extension is unavailable'}
         # explain: second pagination query fails, so after a query we will establish a new connection to the server.
@@ -213,6 +217,31 @@ class ActiveDirectory(object):
     def escaped(query):
         return escape_bytes(query)
 
+    def __as_unicode(self, s):
+        if s:
+            return s.decode('utf-8')
+        else:
+            return s
+
+    def __compress_attributes(self, dic):
+        """
+        This will convert all attributes that are list with only one item string into simple string. It seems that LDAP always return lists, even when it doesn
+        t make sense.
+
+        :param dic:
+        :return:
+        """
+        result = {}
+        for k, v in dic.iteritems():
+            if isinstance(v, types.ListType) and len(v) == 1:
+                if k not in ('msExchMailboxSecurityDescriptor', 'msExchSafeSendersHash',
+                             'objectSid', 'objectGUID', 'msExchArchiveGUID', 'thumbnailPhoto', 'msExchMailboxGuid'):
+                    try:
+                        result[k] = v[0].decode('utf-8')
+                    except Exception as e:
+                        print("FAILED: %s : %s -- %s" % (k,v[0], e))
+        return result
+
     def get_attributes(self, attributes=None, user=None, email=None, name=None):
         if user is None and email is None and name is None:
             raise Exception("How do you expect to get an attribute when you specify no even one of user/email/name?")
@@ -232,19 +261,14 @@ class ActiveDirectory(object):
         res = {}
         self.logger.debug("%s : %s" % (filter, attributes))
         r = self.search_ext_s(filterstr=filter, attrlist=attributes)
-        # Type,user = self.conn.result(r,60)
-        # print Type,user
-        # sys.exit()
-        # print "user:", user
-        # Name,Attrs = user[0]
         if not r:
             return None
+
+        if len(r) != 1:
+            raise NotImplementedError("getAttributes does not support returning values for multiple ldap objects")
+
         # print(r[0])
-        Name, Attrs = r[0]
-        for attribute in attributes:
-            if hasattr(Attrs, 'has_key') and attribute in Attrs:
-                res[attribute] = Attrs[attribute][0]
-        return res
+        return self.__compress_attributes(r[0]['attributes'])
 
     def get_attribute(self, attribute='sAMAccountName', user=None, email=None, name=None, dn=None):
         """
@@ -256,6 +280,7 @@ class ActiveDirectory(object):
         :param dn:
         :return: str
         """
+        filter = "(objectclass=*)"
         if user is None and email is None and name is None and dn is None:
             raise Exception("How do you expect to get an attribute when you specify no even one of user/email/name?")
         if user:
@@ -293,7 +318,7 @@ class ActiveDirectory(object):
             logging.error("xxx")
 
     def get_name(self, user=None):
-        return self.get_attribute('displayName', user=user)
+        return self.__as_unicode(self.get_attribute('displayName', user=user))
 
     def get_username(self, user=None, dn=None):
         if user:
@@ -311,7 +336,7 @@ class ActiveDirectory(object):
         return r[0]['dn']
 
     def get_email(self, user=None):
-        return self.get_attribute(attribute='mail', user=user)
+        return self.__as_unicode(self.get_attribute(attribute='mail', user=user))
 
     def is_user_enabled(self, user):
         ret = None
@@ -339,22 +364,26 @@ class ActiveDirectory(object):
 class ActiveDirectoryTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.size_limit = 10
+        self.paged_size = 2
+        self.time_limit = 60
         # "ldap://ldap.forumsys.com:389", "cn=read-only-admin,dc=example,dc=com", "password"
 
         #directory = "ldap://ldap.forumsys.com:389"
         #self.ad = ActiveDirectory(directory, dn='cn=read-only-admin,dc=example,dc=com', secret='password', size_limit=50)
-        self.ad = ActiveDirectory("ldaps://lonpdc01.citrite.net:3269/citrite,dc=net", size_limit=2)
+        self.ad = ActiveDirectory("ldaps://lonpdc01.citrite.net:3269/citrite,dc=net", size_limit=self.size_limit, paged_size=self.paged_size, time_limit=self.time_limit )
 
         #self.ad = ActiveDirectory("ldaps://pycontribs.onmicrosoft.com:3269", dn="john@pycontribs.onmicrosoft.com", secret="Gunu4138", size_limit=2)
 
     def test_get_name(self):
-        self.assertEqual(self.ad.get_name('sorins'), 'Sorin Sbârnea')
+        name = self.ad.get_name('sorins')
+        self.assertEqual(name, u'Sorin Sbârnea')
 
     def test_get_name2(self):
         # this one tests special characters that do need to be properly escaped
         self.assertEqual(self.ad.get_name('_6363 Conf. 2033 (14'), '_6363 Conf. 2033_The Atrium (14)')
 
-    def test_get_email(self):
+    def test_get_email_invalid(self):
         # getting email of non existing account should return none
         self.assertEqual(self.ad.get_email('xcxsfscbr33g'), None)
 
@@ -365,13 +394,19 @@ class ActiveDirectoryTestCase(unittest.TestCase):
         self.assertEqual(self.ad.get_manager('svcacct_scale'), 'benha')
 
     def test_get_users(self):
-        self.ad.get_users()
+        users = self.ad.get_users()
+        self.assertEqual(len(users), self.size_limit)
 
     def test_is_user_enabled(self):
         self.assertTrue(self.ad.is_user_enabled('sorins'))
 
     def test_is_user_enabled_non_existing(self):
         self.assertTrue(self.ad.is_user_enabled('sdsECGCCgcreRHdrsrdhd') is None)
+
+    def test_get_attributes(self):
+        user = self.ad.get_attributes(user='sorins')
+        self.assertEqual(user['displayName'], u'Sorin Sbârnea')
+        self.assertEqual(user['name'], u'Sorin Sbârnea')
 
 if __name__ == "__main__":
     import sys
--- activedirectory/version.py
+++ activedirectory/version.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
-__version__ = "0.0.4"
-__date__ = "2014-07-09"
+__version__ = "0.0.5"
+__date__ = "2014-07-10"
