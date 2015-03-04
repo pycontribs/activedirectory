cd491511e2ed667c3b4634a682ea012c6cbedb38
sorin.sbarnea@gmail.com <sorin.sbarnea@gmail.com>
2015-03-04 10:10:35 +0000

    Preventing pep8 error E402

--- activedirectory/__init__.py
+++ activedirectory/__init__.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 from __future__ import absolute_import
+import sys
+from . import version
+from .activedirectory import ActiveDirectory
 
 __author__ = "Sorin Sbarnea"
 __copyright__ = "Copyright 2014, Sorin Sbarnea"
 __email__ = "sorin(dot)sbarnea(at)gmail.com"
 __status__ = "Production"
-from . import version
 __date__ = "2014-12-16"
 __all__ = ['version']
 
-import sys
 if sys.hexversion < 0x02050000:
     sys.exit("Python 2.5 or newer is required by tendo module.")
-
-from .activedirectory import ActiveDirectory
